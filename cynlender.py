e5ddc3e7460973b90f438e3e8e764dac1529b9da
yashmali408 <yashmali408@gmail.com>
2023-12-07 15:45:45 +0530

    ym

---
+++ cynlender.py
@@ -0,0 +1,8 @@
+r = input ("pleace enter your radius")
+r = float(r)
+h = input('pleace enter your height')
+h = float(h)
+
+volumeofcyclinder=(22/7)*r*r*h
+
+print (f"the volume of cylinder is {volumeofcyclinder} ")
